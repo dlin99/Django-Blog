b2008e3309502486048e235e8e94c3aa4a2d7bc3
Danny <linycdanny@gmail.com>
2020-11-05 15:40:29 +0800

    checkpoint1

---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Post
+
+# from django.http import HttpResponse
+
+def home(request):
+    # return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    # return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
