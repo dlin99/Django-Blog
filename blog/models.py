c24ad197e6a47b4387dd749f3867fc53a1fbbafe
Danny Lin <linycdanny@gmail.com>
2020-12-23 12:11:02 +0800

    checkpoint for adding comment functionality

--- blog/models.py
+++ blog/models.py
@@ -5,10 +5,11 @@ from django.urls import reverse
 # Create your models here.
 
 class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
 
     def __str__(self):
         return self.title
